bb45f0adabdc10764e2e7ac76863e485d38a104f
Emmanuel Moila <emmanuelmoila@gmail.com>
2019-06-23 00:01:47 +0200

    add "second commit test"

--- src/com/workcompany/Main.java
+++ src/com/workcompany/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Testing Git GUI On IntelliJ IDE");
+        System.out.println("Second commit test");
     }
 }
